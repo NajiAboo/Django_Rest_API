eba38d539417325e826494d7bf5a39f63e5a7068
t0393 <naji.aboo@triassicsolutions.com>
2021-07-19 08:34:21 +0530

    removed the secrete key

--- restapi_udemy/settings.py
+++ restapi_udemy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1-7o^d$z@3_w)z8oik1ut*lg2id5xlfc1hy@uso)j8+dy+gym%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
